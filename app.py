ea568df6c23c759801ce5711a072339a97746318
qianz3335-bit <qianz3335@gmail.com>
2026-05-23 23:20:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -731,6 +731,4 @@ def add_task():
     return jsonify({'status': 'ok', 'tasks': sessions[session_id]['tasks']})
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=False)
